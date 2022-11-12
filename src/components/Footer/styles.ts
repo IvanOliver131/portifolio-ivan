a8327c754bdd0f244dfb41f77326df168e6174c8
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-11-12 14:13:56 -0300

    finish

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,14 +2,21 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
   width: 100%;
-  height: 56px;
   display: flex;
   justify-content: space-around;
   align-items: center;
+
+  padding: 0.5rem;
 
   background-color: ${(props) => props.theme.primary};
 
   div {
     margin-bottom: 0.5rem;
+  }
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    flex-direction: column-reverse;
+
+    gap: 0.5rem;
   }
 `;
